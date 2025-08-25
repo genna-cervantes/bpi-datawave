2367ae241f6a6c2b082a0102f4c18519662d9b5f
genna-cervantes <gennacervantes9@gmail.com>
2025-08-25 10:47:41 +0800

    landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,27 @@
-import { Button } from "@/components/ui/button";
+'use client'
 
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+
+  const router = useRouter();
+  
   return (
     <div className="flex justify-between h-screen overflow-hidden items-center">
-      <div className="text-[var(--main-red)] h-full flex flex-col justify-end items-start px-10 py-20">
+      <div className="h-full flex flex-col justify-end items-start px-10 py-20">
         <div>
-          <h1 className="text-4xl font-bold font-mono">Cashflow Guardian</h1>
-          <p className="text-lg mt-4">
+          <h1 className="text-2xl font-bold font-mono">Cashflow Guardian</h1>
+          <p className=" mt-2">
             Cashflow Guardian is a tool that helps you manage your cashflow. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
           </p>
         </div>
-        <Button className="mt-4 bg-[var(--main-red)] text-white text-md font-mono">Get Started</Button>
+        <Button size='sm' onClick={() => router.push('/assistant')} className="hover:bg-[var(--main-red)] font-semibold cursor-pointer mt-4 bg-gray-100 text-black hover:text-white text-md font-mono">Get Started</Button>
       </div>
-      <div className="bg-[var(--main-red)] h-[80vh] w-[50vw] rounded-l-3xl shrink-0"></div>
+      <div className="bg-[var(--main-red)] h-[75vh] w-[50vw] rounded-l-3xl shrink-0">
+        <Image src="/vc_landing.png" alt="venture_compass" width={1000} height={100} className="w-full h-full object-contain" />
+      </div>
     </div>
   );
 }
